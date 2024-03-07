db65e9728190005c460f0a0589f28b96e872a3b0
DeepBiswas <105808324+DeepBwas@users.noreply.github.com>
2024-03-06 23:48:19 -0500

    Almost Done

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+const output = document.getElementById('hamburger');
+
+function createHamburger(bun, sauce, vegetable, patty, pattyNum, cheese, condiments){
+    let object = {};
+    object.bun = bun;
+    object.sauce = sauce;
+    object.vegetable = vegetable;
+    object.patty = patty;
+    object.pattyNum = pattyNum;
+    object.cheese = cheese;
+    object.condiments = condiments;
+    object.info = function(){
+        return `Your hamburger order contains a ${this.bun} bun, ${this.sauce} sauce, ${this.vegetable}, ${this.pattyNum} ${this.patty} patty, ${this.cheese} cheese, and ${this.condiments} as extra.`;
+    };
+    return object;
+};
+
+const hamburger = createHamburger('sesame', 'special', 'lettuce, tomato, onion', 'beef', 1, 'cheddar', 'ketchup, mustard');
+output.textContent = hamburger.info();
+
